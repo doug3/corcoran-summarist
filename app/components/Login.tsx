392a5b963afdd5c1bf7f0b5dcc8de5f68b448597
doug3 <doug.corcoran@hotmail.com>
2026-01-31 15:05:35 -0500

    Firebase integration update

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,5 +1,11 @@
 "use client";
-
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { auth } from "../firebase";
+import {
+  useCreateUserWithEmailAndPassword,
+  useSendEmailVerification,
+} from "react-firebase-hooks/auth";
 import Link from "next/link";
 import Image from "next/image";
 import { FaUserLarge } from "react-icons/fa6";
@@ -10,6 +16,21 @@ export default function Login({
 }: {
   handleShowModal: () => void;
 }) {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [createUser] = useCreateUserWithEmailAndPassword(auth);
+  const [sendEmailVerification] = useSendEmailVerification(auth);
+
+  const onSubmitCreate = async () => {
+    await createUser(email, password);
+    await sendEmailVerification();
+  };
+
+  const onSubmitLogin = async () => {
+    await createUser(email, password);
+    await sendEmailVerification();
+  };
   return (
     <div
       className="w-full h-full absolute top-0 backdrop-filter backdrop-brightness-75 backdrop-blur-md flex justify-center items-center"
@@ -54,7 +75,10 @@ export default function Login({
             </div>
 
             <div className="mt-7 flex flex-col gap-6">
-              <button id="guest-login" className="inline-flex h-12 w-full text-white justify-center items-center text-center bg-blue-700 hover:bg-blue-900 rounded border border-slate-300 p-2 text-lg font-medium outline-none focus:ring-2 focus:ring-[#333] focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-60">
+              <button
+                id="guest-login"
+                className="inline-flex h-12 w-full text-white justify-center items-center text-center bg-blue-700 hover:bg-blue-900 rounded border border-slate-300 p-2 text-lg font-medium outline-none focus:ring-2 focus:ring-[#333] focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-60"
+              >
                 <FaUserLarge className="absolute left-2 h-8 w-8" />
                 Login as a Guest
               </button>
@@ -65,7 +89,10 @@ export default function Login({
                 <div className="h-px w-full bg-slate-600 "></div>
               </div>
 
-              <button id="google-login" className="inline-flex h-12 w-full items-center justify-center bg-blue-500 hover:bg-blue-700 rounded p-2 text-lg font-medium text-white outline-none focus:ring-2 focus:ring-[#333] focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-60">
+              <button
+                id="google-login"
+                className="inline-flex h-12 w-full items-center justify-center bg-blue-500 hover:bg-blue-700 rounded p-2 text-lg font-medium text-white outline-none focus:ring-2 focus:ring-[#333] focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-60"
+              >
                 <Image
                   width={100}
                   height={100}
@@ -90,6 +117,7 @@ export default function Login({
               <input
                 name="email"
                 type="email"
+                onChange={(e) => setEmail(e.target.value)}
                 autoComplete="email"
                 required
                 className="w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 outline-green-400 mb-4"
@@ -101,6 +129,7 @@ export default function Login({
               <input
                 name="password"
                 type="password"
+                onChange={(e) => setPassword(e.target.value)}
                 autoComplete="current-password"
                 required
                 className="w-full rounded-lg border border-gray-300 px-3 py-2 mt-1! shadow-sm placeholder:text-gray-400 outline-green-400"
@@ -110,27 +139,33 @@ export default function Login({
               <button
                 id="email-login"
                 type="submit"
-                className="inline-flex w-full items-center justify-center rounded-lg bg-green-400 p-2 py-3 mt-4! text-xl font-medium text-black outline-none  disabled:bg-gray-400"
+                onSubmit={onSubmitCreate}
+                className="inline-flex w-50 items-center justify-center rounded-lg bg-fuchsia-300 p-2 py-3 mt-4! mr-2 text-xl font-medium text-black outline-none  disabled:bg-gray-400"
+              >
+                Create New Account
+              </button>
+
+              <button
+                id="email-login"
+                type="submit"
+                onSubmit={onSubmitLogin}
+                className="inline-flex w-50 items-center justify-center rounded-lg bg-green-400 p-2 py-3 mt-4! text-xl font-medium text-black outline-none  disabled:bg-gray-400"
               >
                 Login
               </button>
-              
             </form>
 
             <p className="mb-3! mt-4! text-lg text-gray-500 text-center">
-                <Link
-                  href="/forgot-password"
-                  className="text-blue-800 hover:text-blue-600"
-                >
-                  Forgot your password?
-                </Link>
-              </p>
+              <Link
+                href="/forgot-password"
+                className="text-blue-800 hover:text-blue-600"
+              >
+                Forgot your password?
+              </Link>
+            </p>
 
             <div className="mt-6! text-center text-lg text-blue-800 hover:text-blue-500 bg-slate-300">
-              <Link
-                href="/signup"
-                className="font-medium"
-              >
+              <Link href="/signup" className="font-medium">
                 Don&apos;t have an account?
               </Link>
             </div>
